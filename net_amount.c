cba694ed1a96acb150819e3180305532deae2b8a
srikanthpragada <srikanthpragada@gmail.com>
2024-09-30 17:56:01 +0530

    30-SEP

---
+++ net_amount.c
@@ -0,0 +1,23 @@
+// Program to display net amount with 15% discount
+
+#include <stdio.h>
+
+void main()
+{
+ int price,qty, amount, discount, net_amount;
+
+    printf("Enter qty : ");
+    scanf("%d",&qty);
+
+    printf("Enter price : ");
+    scanf("%d",&price);
+
+    amount = qty * price;
+    discount =  15.0 / 100 * amount;
+
+    net_amount = amount - discount;
+
+    printf("Amount     : %6d\n", amount);
+    printf("- Discount : %6d\n", discount);
+    printf("Net Amount : %6d\n", net_amount);
+}
---
+++ sizes.c
@@ -0,0 +1,12 @@
+// Program to display size of int
+
+#include <stdio.h>
+
+void main()
+{
+
+    printf("Size of short= %d\n", sizeof(short));
+    printf("Size of int  = %d\n", sizeof(int));
+    printf("Size of long = %d", sizeof(long));
+
+}
